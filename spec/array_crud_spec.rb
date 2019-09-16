0849f7b884823c5072cc9a30018acf695224ee97
Kristan Johns <kristandjohns@gmail.com>
2019-09-16 02:35:03 +0000

    Done.

--- spec/array_crud_spec.rb
+++ spec/array_crud_spec.rb
@@ -3,6 +3,9 @@ require "spec_helper"
 describe "#create_an_empty_array" do
   it "creates and returns and empty array" do 
     expect(create_an_empty_array).to eq([])
+ def 
+ create_an_empty_array
+    []
   end 
 end
 
